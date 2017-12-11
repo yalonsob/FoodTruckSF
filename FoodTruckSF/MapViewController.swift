2c8d8efae676a629e8cfece1597b86cf18795b2a
yalonsob <yalonsob@gmail.com>
2017-12-11 11:43:20 -0800

    Tab Bar Navigation

---
+++ FoodTruckSF/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  FoodTruckSF
+//
+//  Created by Yolanda Alonso on 12/11/17.
+//  Copyright © 2017 Yolanda Alonso. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
